807a87f52860f2f0abf4f043af72c1c8a4df2c71
WILLY MAURICIO DORADO DORADO <a2017210277@estudiantes.upsa.edu.bo>
2020-09-19 02:55:11 -0400

    Se incluyó la clase conversores

---
+++ Numeroaletra/Numeroaletra/Controllers/LiteralController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Numeroaletra.Helpers;
+
+namespace Numeroaletra.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LiteralController : ControllerBase
+    {
+        [HttpGet]
+        public string Literalget(int numero)
+        {
+            return Conversores.NumeroALetras(numero);
+        }
+        [HttpPost]
+        public string LiteralPost([FromHeader]int numero)
+        {
+            return Conversores.NumeroALetras(numero);
+        }
+    }
+}
